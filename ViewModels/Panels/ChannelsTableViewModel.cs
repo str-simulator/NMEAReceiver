d5bb70115e87776f59fb23916045f35987999151
Younghun Yoo <yyh7750@strkorea.co.kr>
2026-06-08 09:01:16 +0900

    feat : List 정렬 기능 추가

--- ViewModels/Panels/ChannelsTableViewModel.cs
+++ ViewModels/Panels/ChannelsTableViewModel.cs
@@ -2,8 +2,8 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NMEAReceiver.Services.Interfaces;
 using NMEAReceiver.ViewModels.Shell;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Data;
 
 namespace NMEAReceiver.ViewModels.Panels;
 
@@ -12,11 +12,16 @@ public partial class ChannelsTableViewModel : ObservableObject
     private readonly MainStateStore _store;
     private readonly IReceiverChannelService _channelService;
 
+    public ICollectionView ChannelsView { get; }
+
     public ChannelsTableViewModel(MainStateStore store, IReceiverChannelService channelService)
     {
         _store = store;
         _channelService = channelService;
         _store.PropertyChanged += OnStorePropertyChanged;
+
+        ChannelsView = CollectionViewSource.GetDefaultView(_store.Channels);
+        ChannelsView.SortDescriptions.Add(new SortDescription(nameof(ChannelRowViewModel.PortName), ListSortDirection.Ascending));
     }
 
     [RelayCommand]
@@ -27,8 +32,6 @@ public partial class ChannelsTableViewModel : ObservableObject
         if (e.PropertyName == nameof(MainStateStore.SelectedChannel))
             OnPropertyChanged(nameof(SelectedChannel));
     }
-
-    public ObservableCollection<ChannelRowViewModel> Channels => _store.Channels;
 
     public ChannelRowViewModel? SelectedChannel
     {
--- Views/Panels/ChannelsTableView.xaml.cs
+++ Views/Panels/ChannelsTableView.xaml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace NMEAReceiver.Views.Panels;
@@ -5,4 +7,12 @@ namespace NMEAReceiver.Views.Panels;
 public partial class ChannelsTableView : UserControl
 {
     public ChannelsTableView() => InitializeComponent();
+
+    private void OnDataGridLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not DataGrid dg) return;
+        var portCol = dg.Columns.FirstOrDefault(c => "Port".Equals(c.Header));
+        if (portCol is not null)
+            portCol.SortDirection = ListSortDirection.Ascending;
+    }
 }
